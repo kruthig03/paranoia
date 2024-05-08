2fb109cd80bbcc06c461c466a3a48397abcccc45
jadynpark <jadynpark@uchicago.edu>
2024-05-08 13:39:57 -0500

    Edited description

--- scripts/preprocessing/1_align_pupil.py
+++ scripts/preprocessing/1_align_pupil.py
@@ -1,4 +1,5 @@
 # Authors: Kruthi Gollapudi (kruthig@uchicago.edu), Jadyn Park (jadynpark@uchicago.edu)
 # Last Edited: May 8, 2024
-# Description The script aligns pupil data to stimulus presentation and 
-# excludes noisy participants.
+# Description: The script aligns pupil data to stimulus presentation and excludes noisy participants
+# A participant is excluded if 25% of the raw samples 
+# (1) contain amplitudes of <1.5mm or (2) the change in amplitude is >0.075mm relative to the directly preceding sample (Murphy et al., 2014)
