91b3aab96f677e6be5f9eadaa7e955fb085bbfa4
Kruthi <kruthig@uchicago.edu>
2024-06-06 15:15:41 -0500

    final script used

--- scripts/preprocessing/1_align_pupil.py
+++ scripts/preprocessing/1_align_pupil.py
@@ -1,19 +1,14 @@
 # Authors: Kruthi Gollapudi (kruthig@uchicago.edu), Jadyn Park (jadynpark@uchicago.edu)
 # Last Edited: May 8, 2024
-<<<<<<< Updated upstream
-# Description: The script aligns pupil data to stimulus presentation and excludes noisy participants
-# A participant is excluded if 25% of the raw samples 
-# (1) contain amplitudes of <1.5mm or (2) the change in amplitude is >0.075mm relative to the directly preceding sample (Murphy et al., 2014)
-=======
-# Description The script aligns pupil data to stimulus presentation and 
-# excludes noisy participants.
+# Description: The script aligns pupil data to stimulus presentation and excludes non-encoding data
+
 
 import numpy as np
 import pandas as pd
 import os
 import scipy.io as sio
+import mat73
 import math
-
 
 
 def fetch_mat(path, subj):
@@ -27,8 +22,8 @@ def fetch_mat(path, subj):
         Detailed description of the variables: http://sr-research.jp/support/EyeLink%201000%20User%20Manual%201.5.0.pdf
     
     """
-    
-    mat = sio.loadmat(os.path.join(path, str(subj), str(subj) + "_ET.mat"))
+
+    mat = mat73.loadmat(os.path.join(path, str(subj), str(subj) + "_ET.mat"))
     samples = mat['Samples']
     events = mat['Events']
     
@@ -36,17 +31,19 @@ def fetch_mat(path, subj):
 
 
 # Set data directories
-_thisDir = os.getcwd()
-mat_path = os.path.normpath(os.path.join(_thisDir, '..', '..', '..', 'pupil',  'data', '2_mat'))
-ts_path = os.path.normpath(os.path.join(_thisDir, '..', '..', 'data'))
+#_thisDir = os.getcwd()
+mat_path = os.path.normpath('/Users/kruthigollapudi/src/paranoia/data/pupil/2_mat')
+ts_path = os.path.normpath('/Users/kruthigollapudi/src/paranoia/data/timestamps')
 
 # Set save directory
-save_path = os.path.normpath(os.path.join(mat_path, '..', '3_processed', '1_blinks_removed'))
+save_path = os.path.normpath('/Users/kruthigollapudi/src/paranoia/data/pupil/3_processed/1_aligned')
+
+
 if not os.path.exists(save_path):
     os.makedirs(save_path)
 
 # Set range of subjects
-subj_ids = range(1002, 1031)
+subj_ids = range(1002, 1016)
 
 # Align pupil data to stimulus
 
@@ -56,41 +53,32 @@ for sub in subj_ids:
     timestamps = pd.read_csv(os.path.join(ts_path, str(sub) + "_paranoia_timestamps.csv"))
     samples, events = fetch_mat(mat_path, sub)
 
-     # Time stamp of samples
-    time = samples['time'][0,0]
+    # Time stamp of samples
+    time = samples['time']
     
     # Pupil size during the entire timecourse
-    pupilSize = samples['pupilSize'][0,0]
+    pupilSize = samples['pupilSize']
 
     # story start and end
     story_start = timestamps["storyStart"][0]
     story_end = timestamps["storyEnd"][0]
-
-    ## first, create sample array that is aligned to stimulus onset and offset
     
     # get start and end indexes
     f_sample = int(500) # Sampling frequency/rate(Hz)
     start_idx = int(math.floor(story_start * f_sample))
-    end_idx = int(math.ceil(story_start * f_sample))
+    end_idx = int(math.ceil(story_end * f_sample))
 
     # new array of only samples during stimulus presentation
-    pupilSize_encoding = pupilSize[start_idx, end_idx]
+    pupilSize_encoding = pupilSize[start_idx: end_idx]
+    pupil_time = time[start_idx: end_idx]
+
+    sample_num = len(pupilSize_encoding)
+    stim_length = sample_num / 30000
 
     # number of samples
-    nSample_orig = len(pupilSize)
-    nSample = len(pupilSize_encoding)
+    #nSample_orig = len(pupilSize)
+    #nSample = len(pupilSize_encoding)
+    
+    filename = os.path.join(save_path, str(sub) + "_aligned_ET.mat")
+    sio.savemat(filename, {'pupilEncoding':pupilSize_encoding, 'time': pupil_time, 'sample_num': sample_num, 'stim_min': stim_length})
 
-    # create array to keep track of noisy data
-    noise = np.array([])
-
-    ## second, filter out samples with excess noise
-
-
-        
-    # Save data mat file if no excessive noise found
-
-    if len(noise) / nSample >= 0.25:
-        continue
-    else:
-        filename = os.path.join(save_path, str(sub) + "SAVEFILE HERE.mat")
->>>>>>> Stashed changes
