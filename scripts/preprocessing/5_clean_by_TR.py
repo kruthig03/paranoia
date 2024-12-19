e439991f220a6b385604fef52e9aa67bfd156f01
jadynpark <jadynpark@uchicago.edu>
2024-12-19 13:25:53 -0600

    updated script

--- scripts/preprocessing/5_clean_by_TR.py
+++ scripts/preprocessing/5_clean_by_TR.py
@@ -1,33 +1,35 @@
 # Authors: Kruthi Gollapudi (kruthig@uchicago.edu), Jadyn Park (jadynpark@uchicago.edu)
-# Last Edited: June 12, 2024
-# Description: This script segments data into 1 second epochs (1 TR) and averages and removes data +/- 1 SD from segment mean
-# If 40% (changed to 50% for now) of the epoch data had to be removed, then interpolates across previous and following segments. (Murphy 2014)
+# Last Edited: December 19, 2024
+# Description: This script performs steps (3) and (4) described in Murphy et al., 2014 (Hum. Brain Mapp.)
+#              Essentially, it removes noisy samples while performing downsampling to align pupil data to brain data
+
+# From Murphy et al., (2014): "(3) Data were segmented into epochs from 0 to +2 s relative to the acquisition onset of each fMRI volume. 
+#                              Within each epoch, amplitude (any sample < 1.5 mm) and variability (any sample ± 3 s.d. outside the epoch mean) thresholds 
+#                              were applied to identify artefactual samples which survived Step 2. 
+#                              An average pupil diameter measure was then calculated for the corresponding volume by taking the mean across 
+#                              the remaining non-artifactual samples in that epoch. 
+#                              This step is equivalent to time-locking the continuous pupil data to the onset of fMRI data acquisition
+#                              and downsampling to the temporal resolution of the EPI sequence (0.5 Hz) using only clean data samples. 
+#                              (4) Mean pupil diameter for any epoch characterized by >40% artifactual samples was replaced 
+#                               via linear interpolation across adjacent clean epochs."
+
+# Steps:
+# 1. Load downsampled (50 Hz; i.e., sampled every 20 ms) pupil data
+# 2. The TR for the brain data is 1 second. To align the pupil data to TRs, segment the data into 1-second epochs
+#    (i.e., 1 epoch = 50 samples)
+# 3. Identify the artifactual samples within each epoch by removing samples that are ± 3 s.d. outside the epoch mean
+# 4. Calculate the mean pupil diameter for each epoch from the remaining non-artifactual samples
+# 5. If an epoch is characterized by >40% artifactual samples, 
+#    replace the mean pupil diameter for that epoch via linear interpolation across adjacent clean epochs
 
 import numpy as np
 import pandas as pd
 import os
-import scipy.io as sio
 import math
 import importlib
+import scipy.stats as stats
 
-module_name = '3_interpolate_blinks'
-module = importlib.import_module(module_name)
-zero_runs = getattr(module, 'zero_runs')
-interpolate_blinks = getattr(module, 'interpolate_blinks')
-
-# Set data directories
-mat_path = os.path.normpath('/Users/kruthigollapudi/src/paranoia/data/pupil/3_processed/4_downsampled')
-ts_path = os.path.normpath('/Users/kruthigollapudi/src/paranoia/data/timestamps')
-
-# Set save directory
-save_path = os.path.normpath('/Users/kruthigollapudi/src/paranoia/data/pupil/3_processed/5_last_interp')
-
-if not os.path.exists(save_path):
-    os.makedirs(save_path)
-
-# Set range of subjects
-subj_ids = range(1002, 1023)
-
+# ------------------ Define functions ------------------ # 
 def compute_epoch_noise(arr, mean, interval, prop):
     '''
     Determines if segment of data is considered noisy, which is when 40% or more of the samples
@@ -62,14 +64,33 @@ def compute_epoch_noise(arr, mean, interval, prop):
     
     return result
 
+# ------------------ Hardcoded parameters ------------------ #
+os.chdir('/Users/jadyn/repo/paranoia/scripts/preprocessing')
+_THISDIR = os.getcwd()
+DAT_PATH = os.path.normpath(os.path.join(_THISDIR, '../../data/pupil/3_processed/4_downsampled'))
+SAVE_PATH = os.path.normpath(os.path.join(_THISDIR, '../../data/pupil/3_processed/5_timelocked'))
+
+if not os.path.exists(SAVE_PATH):
+    os.makedirs(SAVE_PATH)
 
 f_sample = 50  # sampling rate (downsampled to)
+module_name = '3_interpolate_blinks'
+module = importlib.import_module(module_name)
+zero_runs = getattr(module, 'zero_runs')
+interpolate_blinks = getattr(module, 'interpolate_blinks')
 
-for sub in subj_ids:
+SUBJ_IDS = range(1002, 1005)
+
+# ------------------- Main ------------------ #
+for sub in SUBJ_IDS:
     
-    # fetch data
-    mat = sio.loadmat(os.path.join(mat_path, str(sub) + "_downsampled_ET.mat"))
-    pupilSize = mat['pupilDownsampled'].flatten()
+    # Load clean pupil data
+    file_path = os.path.join(DAT_PATH, str(sub) + "_downsampled_ET.csv")
+    if not os.path.exists(file_path):
+        continue
+    dat = pd.read_csv(file_path)
+    
+    
 
     n = len(pupilSize)
     epoch_set = np.array([])
